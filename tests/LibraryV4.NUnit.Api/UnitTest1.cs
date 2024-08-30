6b9ef61bec70225840aaadf7b95c821e92145f5b
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-08-29 22:25:33 -0700

    Add homework 13

---
+++ tests/LibraryV4.NUnit.Api/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace LibraryV4.NUnit.Api;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
