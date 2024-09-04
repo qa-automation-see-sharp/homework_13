9df85213bd4a10fa6c450dbfea9e140064412f04
Sergii Zvonov <s.zvonov@gmail.com>
2024-09-04 21:51:55 +0300

    finish copying tests

--- tests/LibraryV4.NUnit.Api/Tests/DeleteBookTests.cs
+++ tests/LibraryV4.NUnit.Api/Tests/DeleteBookTests.cs
@@ -1,8 +1,34 @@
-using System;
+using System.Net;
+using LibraryV4.NUnit.Api.Fixtures;
+using static LibraryV4.NUnit.Api.TestHelpers.DataHelper;
 
 namespace LibraryV4.NUnit.Api.Tests;
 
-public class DeleteBookTests
+public class DeleteBookTests : LibraryV4TestFixture
 {
+    [Test]
+    public async Task DeleteBook_ShouldReturnOK()
+    {
+        //Arrange
+        var book = CreateBook();
+        await LibraryHttpService.PostBook(book);
 
+        //Act
+        var response = await LibraryHttpService.DeleteBook(book.Title, book.Author);
+
+        //Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task DeleteBook_NotExistingBook_ShouldReturnNotFound()
+    {
+        //Arrange - N/A
+
+        //Act
+        var httpResponseMessage = await LibraryHttpService.DeleteBook(Guid.NewGuid().ToString(), Guid.NewGuid().ToString());
+
+        //Assert
+        Assert.That(httpResponseMessage.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
 }
--- tests/LibraryV4.xUnit.Tests.Api/TestHelpers/DataHelper.cs
+++ tests/LibraryV4.xUnit.Tests.Api/TestHelpers/DataHelper.cs
@@ -1,8 +1,34 @@
-using System;
+using LibraryV4.Contracts.Domain;
 
 namespace LibraryV4.xUnit.Tests.Api.TestHelpers;
 
 public class DataHelper
 {
-
+    public static Book CreateBook()
+    {
+        return new Book
+        {
+            Title = Guid.NewGuid().ToString(),
+            Author = Guid.NewGuid().ToString(),
+            YearOfRelease = new Random().Next(1850, 2024)
+        };
+    }
+    public static Book CreateBook(string title, string author)
+    {
+        return new Book
+        {   
+            Title = title,
+            Author = author,
+            YearOfRelease = new Random().Next(1850, 2024)
+        };
+    }
+    public static User CreateUser()
+    {
+        return new User 
+        {
+            NickName = Guid.NewGuid().ToString(),
+            Password = Guid.NewGuid().ToString(),
+            FullName = Guid.NewGuid().ToString()
+        };
+    }
 }
