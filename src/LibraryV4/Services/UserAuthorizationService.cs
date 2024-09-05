4ce8add37c45ff42a9e3b85140a31e50bccb373e
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-09-05 12:38:35 -0700

    fix date time issue

--- src/LibraryV4/Services/UserAuthorizationService.cs
+++ src/LibraryV4/Services/UserAuthorizationService.cs
@@ -26,7 +26,7 @@ public class UserAuthorizationService : IUserAuthorizationService
         var token = await _authorizationTokenRepository.GetToken(authorizationToken);
 
         if (token is null) return false;
-        if (token.ExpirationTime <= DateTime.Now) return false;
+        if (token.ExpirationTime <= DateTime.UtcNow) return false;
 
         var user = await _userRepository.GetUser(u => u.Id == token.UserId);
 
@@ -60,7 +60,7 @@ public class UserAuthorizationService : IUserAuthorizationService
         var token = new AuthorizationToken
         {
             Token = Guid.NewGuid(),
-            ExpirationTime = DateTime.Now.AddMinutes(15)
+            ExpirationTime = DateTime.UtcNow.AddMinutes(15)
         };
 
         await _authorizationTokenRepository.AddToken(token.ToDto(userId));
